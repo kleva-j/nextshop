13a0088187ef6c59459f0a9cf2ff970022d9e680
kleva-j <kasmickleva@gmail.com>
2024-03-07 00:24:55 +0100

    update eslint import sort rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,24 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'no-console': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          ['^(@|components)(/.*|$)'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.?(css)$'],
+        ],
+      },
+    ],
   },
 };
