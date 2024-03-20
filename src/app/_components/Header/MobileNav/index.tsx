970432b9eb3e87c5a00ace15a593a257e456f945
kleva-j <kasmickleva@gmail.com>
2024-03-20 15:01:10 +0100

    feat: design mobile nav

--- src/app/_components/Header/MobileNav/index.tsx
+++ src/app/_components/Header/MobileNav/index.tsx
@@ -1,13 +1,36 @@
+'use client';
+
 import type { FC } from 'react';
+import Link from 'next/link';
+
+import { useAuth } from '@/app/_providers/Auth';
+import { Button } from '@/components/ui/button';
 
 import { Header } from '../../../../payload/payload-types';
 
 export const MobileNav: FC<{ header: Header }> = ({ header }) => {
   const navItems = header?.navItems || [];
+  const { user } = useAuth();
 
   return (
-    <button className="border rounded-md px-4 py-2 border-slate-300 bg-transparent text-slate-400">
-      index
-    </button>
+    <nav className="flex items-center flex-wrap gap-x-2">
+      {navItems.map(({ link }, i) => (
+        <Button key={i} variant="link" className="text-base font-normal px-2">
+          {link.label}
+        </Button>
+      ))}
+      <Button variant="link" className="text-base font-normal">
+        Cart
+      </Button>
+      {user && <Link href="/account">Account</Link>}
+      {!user && (
+        <Button
+          className="rounded-none uppercase font-normal bg-gray-950 text-white"
+          onClick={() => (window.location.href = '/login')}
+        >
+          Login
+        </Button>
+      )}
+    </nav>
   );
 };
